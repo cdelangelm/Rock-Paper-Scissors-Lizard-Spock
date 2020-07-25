92c822f5622d87a884b55a17b806bf2c75fad645
cdelangelm <cdelangelm@users.noreply.github.com>
2020-07-24 19:57:40 -0500

    Add project files.

---
+++ Rock-Paper-Scissors-Lizard-Spock.cpp
@@ -0,0 +1,14 @@
+// Rock-Paper-Scissors-Lizard-Spock project from the conditional and logic block lesson 
+// Codecademy
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // Live long and prosper
+    srand(time(NULL));
+    int computer = rand() % 3 + 1;
+    int user = 0;
+}
